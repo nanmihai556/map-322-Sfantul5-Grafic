faa5d9ae34e13d451f4d26708e685b80af5369fe
pamintandrei <42819603+pamintandrei@users.noreply.github.com>
2021-12-28 19:19:35 +0200

    Hide encryption key

--- proiectExtinsV1/src/main/java/socialnetwork/domain/PasswordEncryptor.java
+++ proiectExtinsV1/src/main/java/socialnetwork/domain/PasswordEncryptor.java
@@ -5,7 +5,7 @@ import java.security.MessageDigest;
 public class PasswordEncryptor {
     public static String encryptPassword(String password){
         try{
-        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        MessageDigest messageDigest = MessageDigest.getInstance("You would like to know, wouldn't you?");
         messageDigest.update(password.getBytes());
         return new String(messageDigest.digest());
         }
